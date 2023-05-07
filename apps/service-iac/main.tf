d4a54a9e46f7c538c73bf6ad161c60fb040ce1d7
Calil Amaral <amaral.calil@gmail.com>
2023-05-07 16:32:42 -0300

    fix(DIS-522): configure terraform state storage and retrieval (#4)
    * chore: add fake endpoint
    
    * fix: comment release step to debug and change branch
    
    * feat: add gcp storage to store and retrieve terraform state
    
    * feat: fix action branch
    
    * fix: adjust gcp sdk version
    
    * fix: adjust gcp sdk auth|
    
    * fix: add credentials
    
    * fix: remove with
    
    * fix: adjust credentials in gcs backend
    
    * fix: adjust credentials
    
    * fix: add run time env and uncomment steps

--- apps/service-iac/main.tf
+++ apps/service-iac/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket = "peerlab-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
 # Configure the Fly provider
 provider "fly" {
   useinternaltunnel    = true
@@ -90,6 +97,9 @@ resource "fly_machine" "micro_app_machine_01" {
   region = each.value
   name   = "${local.app_name}-${each.value}"
   image  = "registry.fly.io/${local.app_name}:${local.image_tag}"
+  # env = {
+  #   DATABASE_PROVIDER = "value"
+  # }
   services = [
     {
       ports = [
