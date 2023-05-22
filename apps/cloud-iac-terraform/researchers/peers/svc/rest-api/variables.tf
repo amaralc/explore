6fa754ab716abc532e9db8e9a24ceff41c4eb568
amaralc <amaral.calil@gmail.com>
2023-05-22 02:09:09 -0300

    adjust: use same variables

--- apps/cloud-iac-terraform/researchers/peers/svc/rest-api/variables.tf
+++ apps/cloud-iac-terraform/researchers/peers/svc/rest-api/variables.tf
@@ -1,3 +1,11 @@
+# Define the variables that will be used within the configuration
+
+variable "credentials_path" {
+  description = "The path to the JSON key file for the Service Account Terraform will use to authenticate"
+  type        = string
+  default     = "credentials.json"
+}
+
 variable "project_id" {
   description = "The Google Cloud project ID"
   type        = string
