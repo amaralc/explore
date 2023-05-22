b156dca5921593c4dedd07306a804b45d25720ad
amaralc <amaral.calil@gmail.com>
2023-05-22 02:11:45 -0300

    Revert "refactor: separate files"
    This reverts commit 72019ed61be99ec9a6421c527f50c1702c9b00ab.

--- apps/cloud-iac-terraform/variables.tf
+++ apps/cloud-iac-terraform/variables.tf
@@ -1,11 +1,4 @@
 # Define the variables that will be used within the configuration
-
-variable "credentials_path" {
-  description = "The path to the JSON key file for the Service Account Terraform will use to authenticate"
-  type        = string
-  default     = "credentials.json"
-}
-
 variable "project_id" {
   description = "The Google Cloud project ID"
   type        = string
@@ -14,4 +7,38 @@ variable "project_id" {
 variable "region" {
   description = "The region where resources will be created"
   type        = string
+
+}
+variable "repo_owner" {
+  description = "The GitHub owner's username"
+  type        = string
+  default     = "amaralc"
+}
+variable "repo_name" {
+  description = "The name of the source repository"
+  type        = string
+  default     = "peerlab"
+}
+
+variable "credentials_path" {
+  description = "The path to the JSON key file for the Service Account Terraform will use to authenticate"
+  type        = string
+  default     = "credentials.json"
+}
+
+variable "environment" {
+  description = "The deployment environment (staging, production, etc.)"
+  type        = string
+}
+
+variable "database_url" {
+  description = "The database URL connection string"
+  type        = string
+  sensitive   = true
+}
+
+variable "direct_url" {
+  description = "The direct URL string"
+  type        = string
+  sensitive   = true
 }
