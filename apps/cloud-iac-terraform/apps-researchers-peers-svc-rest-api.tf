3aacfb3a68a3cbe76bfd2d47b8e4c57f081ac3d3
amaralc <amaral.calil@gmail.com>
2023-05-27 12:47:57 -0300

    adjust: remove need for getting commit hash

--- apps/cloud-iac-terraform/apps-researchers-peers-svc-rest-api.tf
+++ apps/cloud-iac-terraform/apps-researchers-peers-svc-rest-api.tf
@@ -1,27 +1,8 @@
 # Define local variables
 locals {
-  service_folder_path = "apps/researchers/peers/svc"  # The path to the Dockerfile from the root of the repository
-  app_name            = "researchers-peers-svc"       # The name of the application
-  app_component_name  = "rest-api"                    # The name of the application component
-  commit_hash_file    = "${path.module}/.commit_hash" # The file where the commit hash will be stored
-}
-
-# Fetch the current commit hash and write it to a file
-resource "null_resource" "get_commit_hash" {
-  provisioner "local-exec" {
-    command = "git rev-parse HEAD > ${local.commit_hash_file}"
-  }
-}
-
-# Read the commit hash from the file
-data "local_file" "commit_hash" {
-  filename   = local.commit_hash_file
-  depends_on = [null_resource.get_commit_hash]
-}
-
-# Trim the commit hash and store it in a local variable (for use in cloud run)
-locals {
-  image_tag = trimspace(data.local_file.commit_hash.content)
+  service_folder_path = "apps/researchers/peers/svc" # The path to the Dockerfile from the root of the repository
+  app_name            = "researchers-peers-svc"      # The name of the application
+  app_component_name  = "rest-api"                   # The name of the application component
 }
 
 # This block creates a new service account
