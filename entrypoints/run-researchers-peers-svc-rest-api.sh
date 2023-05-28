8f730b9bb598164282afaeb623c9a1c6884f6d5f
amaralc <amaral.calil@gmail.com>
2023-05-28 16:12:10 -0300

    adjust: fix entrypoint

--- entrypoints/run-researchers-peers-svc-rest-api.sh
+++ entrypoints/run-researchers-peers-svc-rest-api.sh
@@ -1,2 +1,2 @@
 npx prisma migrate deploy --schema libs/researchers/peers/adapters/src/database/infra/prisma/postgresql.schema.prisma
-node dist/apps/researchers/peers/svc/rest-api/main.js
+node dist/apps/researchers/peers/svc-rest-api/main.js
