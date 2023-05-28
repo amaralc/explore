b2464bda72fa2c41f776d57edd3f42e80229642e
amaralc <amaral.calil@gmail.com>
2023-05-28 14:08:54 -0300

    Revert "refactor: move rest api"
    This reverts commit 016334c34cd3c2cc11510812f8344f7ee8aba04d.

--- apps/researchers/peers/svc-rest-api/jest.config.ts
+++ apps/researchers/peers/svc-rest-api/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'researchers-peers-rest-api-svc',
+  displayName: 'researchers-peers-svc-rest-api',
   preset: '../../../../jest.preset.js',
   globals: {},
   testEnvironment: 'node',
@@ -13,5 +13,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/apps/researchers/peers/rest-api-svc',
+  coverageDirectory: '../../../../coverage/apps/researchers/peers/svc-rest-api',
 };
