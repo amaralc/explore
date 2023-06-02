71b6dbdb93c418c81b4970090ec40ffb37b60951
amaralc <amaral.calil@gmail.com>
2023-06-02 20:45:53 -0300

    chore: organize script

--- scripts/gcloud/create-service-account.sh
+++ scripts/gcloud/create-service-account.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# How to call this script: bash ./file/path/filename.sh
+# Creating a service account:
+# 1. Set the environment variables
+# 2. Call this script with the following command: bash ./scripts/gcloud/create-service-account.sh
 
 SERVICE_ACCOUNT_NAME="my-service-account"
 DESCRIPTION="my-service-account-description"
 DISPLAY_NAME="my-service-account-display-name"
 PROJECT_ID="my-project-id"
 
-# gcloud iam service-accounts create [SERVICE_ACCOUNT_NAME] --description="[DESCRIPTION]" --display-name="[DISPLAY_NAME]" --project=[PROJECT_ID]
 gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME --description="$DESCRIPTION" --display-name="$DISPLAY_NAME" --project=$PROJECT_ID
