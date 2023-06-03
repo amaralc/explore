3100c8f1baa930da092ea9b394fc576ee43924a8
amaralc <amaral.calil@gmail.com>
2023-06-03 11:01:56 -0300

    feat(PEER-543): add script for creating iam bidings

---
+++ scripts/gcloud/service-account-add-iam-policy-bindings.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Variables
+PROJECT_ID="my-project-id"
+SERVICE_ACCOUNT_EMAIL="my-service-account@my-project-id.iam.gserviceaccount.com"
+
+# This script assigns necessary roles to a service account for managing GCP resources with Terraform
+
+# Assign the Secret Manager Secret Accessor role
+# This role allows the service account to access secrets in Secret Manager
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/secretmanager.secretAccessor"
+
+# Assign the IAM Service Account User role
+# This role allows the service account to act as other service accounts
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/iam.serviceAccountUser"
+
+# Assign the Cloud Run Admin role
+# This role allows the service account to administer Cloud Run services
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/run.admin"
+
+# Assign the Cloud Run Invoker role
+# This role allows the service account to invoke Cloud Run services
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/run.invoker"
+
+# Assign the Storage Admin role
+# This role allows the service account to administer Cloud Storage resources
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/storage.admin"
+
+# Assign the Service Account Key Admin role
+# This role allows the service account to create and manage service account keys
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/iam.serviceAccountKeyAdmin"
+
+# Assign the IAM Role Admin role
+# This role allows the service account to create and manage IAM roles
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" --role="roles/iam.roleAdmin"
