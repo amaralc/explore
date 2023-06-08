766a00149d6cd128a29286a6d88c56d4db09fe15
amaralc <amaral.calil@gmail.com>
2023-06-08 15:40:14 -0300

    feat(PEER-542): add nx ignore

---
+++ apps/researchers/peers/svc-rest-api/scripts/affected.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Vercel ignored build steps: https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel
+# Import and execute script (https://stackoverflow.com/questions/12815774/importing-functions-from-a-shell-script)
+source tools/build/nx-ignore.sh
+
+# Call function defined in tools/vercel/nx-ignore.sh to set affected apps and libs
+setAffectedAppsAndLibs
+
+# Set env var with the app or lib you wish to verify
+PROJECT_NAME=researchers-peers-svc-rest-api
+
+# If string with the name of the app is a substring of AFFECTED_APPS string then proceed with the build
+if [[ $AFFECTED_APPS == *$PROJECT_NAME* ]]; then
+  echo '✅ - App was affected!'
+  exit 0
+else
+  # If no dependency was affected, cancel build
+  echo '🛑 - App was not affected'
+  exit 1
+fi
