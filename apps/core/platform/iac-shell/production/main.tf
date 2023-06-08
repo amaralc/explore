9f52189142c3a74285b5a16b171b2e32103ba0a5
amaralc <amaral.calil@gmail.com>
2023-06-08 12:53:47 -0300

    debug: use peerlab repo

--- apps/core/platform/iac-shell/production/main.tf
+++ apps/core/platform/iac-shell/production/main.tf
@@ -12,7 +12,6 @@ resource "vercel_project" "nextjs-app" {
     production_branch = "production"
   }
 
-
   build_command    = "npx nx build nextjs-app --prod"
   output_directory = "dist/apps/nextjs-app/.next"
   dev_command      = "npx nx serve nextjs-app"
@@ -40,8 +39,49 @@ resource "vercel_deployment" "nextjs-app-production" {
   project_id        = vercel_project.nextjs-app.id
   production        = true
   delete_on_destroy = true
-  ref               = "main"
+  ref               = "production"
 }
+
+
+# resource "vercel_project" "nextjs-app" {
+#   name      = "nextjs-app"
+#   framework = "nextjs"
+#   git_repository = {
+#     type              = "github"
+#     repo              = "amaralc/peerlab"
+#     production_branch = "production"
+#   }
+
+
+#   build_command    = "npx nx build nextjs-app --prod"
+#   output_directory = "dist/apps/nextjs-app/.next"
+#   dev_command      = "npx nx serve nextjs-app"
+# }
+
+# # An environment variable that will be created
+# # for this project for the "production" environment.
+# resource "vercel_project_environment_variable" "my-env-var-production" {
+#   project_id = vercel_project.nextjs-app.id
+#   key        = "MY_ENV_VAR"
+#   value      = "value-production"
+#   target     = ["production"]
+# }
+
+# # An environment variable that will be created
+# # for this project for the "production" environment.
+# resource "vercel_project_environment_variable" "my-env-var-preview" {
+#   project_id = vercel_project.nextjs-app.id
+#   key        = "MY_ENV_VAR"
+#   value      = "value-preview"
+#   target     = ["preview", "development"]
+# }
+
+# resource "vercel_deployment" "nextjs-app-production" {
+#   project_id        = vercel_project.nextjs-app.id
+#   production        = true
+#   delete_on_destroy = true
+#   ref               = "production"
+# }
 
 
 # # Application Shell
