af0977eed2f0f24ad77fb74e906acb973fef985a
amaralc <amaral.calil@gmail.com>
2023-06-08 15:54:59 -0300

    docs: add comments

--- apps/researchers/peers/svc-rest-api/scripts/affected.sh
+++ apps/researchers/peers/svc-rest-api/scripts/affected.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Checks weather the project was affected (true) or not (false) logging the result to the console
+
+# Usage:
+# bash apps/researchers/peers/svc-rest-api/scripts/affected.sh > /dev/null && echo 'true' || echo 'false'
+
 # Vercel ignored build steps: https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel
 # Import and execute script (https://stackoverflow.com/questions/12815774/importing-functions-from-a-shell-script)
 source tools/build/nx-ignore.sh
@@ -13,9 +18,9 @@ PROJECT_NAME=researchers-peers-svc-rest-api
 # If string with the name of the app is a substring of AFFECTED_APPS string then proceed with the build
 if [[ $AFFECTED_APPS == *$PROJECT_NAME* ]]; then
   echo '✅ - App was affected!'
-  exit 0
+  exit 0 # Success (true)
 else
   # If no dependency was affected, cancel build
   echo '🛑 - App was not affected'
-  exit 1
+  exit 1 # Failure (false)
 fi
