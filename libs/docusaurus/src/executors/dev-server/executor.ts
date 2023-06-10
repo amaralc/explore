5c9ff8f6823ea4a71708a2a96fa0a454f16c8634
amaralc <amaral.calil@gmail.com>
2023-06-10 13:29:53 -0300

    feat(PEER-542): update schema.json for executors

---
+++ libs/docusaurus/src/executors/dev-server/executor.ts
@@ -0,0 +1,27 @@
+// Reference: https://github.com/ZachJW34/nx-plus/blob/master/libs/docusaurus/src/executors/dev-server/executor.ts
+
+import { start } from '@docusaurus/core/lib';
+import { ExecutorContext } from '@nrwl/devkit';
+import * as path from 'path';
+import { DevServerExecutorSchema } from './schema';
+
+export default async function* runExecutor(options: DevServerExecutorSchema, context: ExecutorContext) {
+  const projectRoot = path.join(context.root, context.workspace.projects[context.projectName ?? ''].root);
+  const port = options.port.toString();
+
+  await start(projectRoot, {
+    port,
+    host: options.host,
+    hotOnly: options.hotOnly,
+    open: options.open,
+  });
+
+  yield {
+    baseUrl: `http://localhost:${port}`,
+    success: true,
+  };
+
+  // This Promise intentionally never resolves, leaving the process running
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  await new Promise<{ success: boolean }>(() => {});
+}
