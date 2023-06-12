efd1095893b8d0d1118d0cdc8b4a06982bbb7a74
amaralc <amaral.calil@gmail.com>
2023-06-12 02:50:18 -0300

    chore: remove nextra docs

--- apps/dx/docs-nextra/theme.config.tsx
+++
@@ -1,9 +0,0 @@
-const themeConfig = {
-  project: {
-    link: 'https://github.com/amaralc/peerlab/blob/production/apps/docs',
-  },
-  logo: <strong>Peerlab</strong>,
-  docsRepositoryBase: 'https://github.com/amaralc/peerlab/blob/production/apps/docs/pages',
-};
-
-export default themeConfig;
