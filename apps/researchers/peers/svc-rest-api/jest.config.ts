214d7066a2d6e86bbefbd50961335664912e7428
amaralc <amaral.calil@gmail.com>
2023-06-13 19:55:08 -0300

    Revert "refactor: move project to researchers-peers-svc-rest-api"
    This reverts commit a96879276f712b6b0ee7c0a27ca29634ae7116f8.

--- apps/researchers/peers/svc-rest-api/jest.config.ts
+++ apps/researchers/peers/svc-rest-api/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'researchers-peers-svc-rest-api',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   globals: {},
   testEnvironment: 'node',
   transform: {
@@ -13,5 +13,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/apps/researchers/peers-svc-rest-api',
+  coverageDirectory: '../../../../coverage/apps/researchers/peers/svc-rest-api',
 };
