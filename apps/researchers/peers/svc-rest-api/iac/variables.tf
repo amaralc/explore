e481bb320a21e6ef2483d2ad1de7453191a58a12
amaralc <amaral.calil@gmail.com>
2023-06-15 23:57:24 -0300

    Revert "fix(PEER-550): use correct image name"
    This reverts commit 18b005454aae452bf03c45437b9031ff0c4d7f8f.

--- apps/researchers/peers/svc-rest-api/iac/variables.tf
+++ apps/researchers/peers/svc-rest-api/iac/variables.tf
@@ -1,11 +1,13 @@
 variable "app_name" {
   description = "The name of the application"
   type        = string
+  default     = "researchers-peers"
 }
 
 variable "app_component_name" {
   description = "The name of the application component"
   type        = string
+  default     = "svc-rest-api"
 }
 
 variable "environment" {
