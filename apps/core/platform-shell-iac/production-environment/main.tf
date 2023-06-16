edad29af64a3688f6a783d8c18d410facb865387
amaralc <amaral.calil@gmail.com>
2023-06-16 00:53:37 -0300

    adjust: use tags to build

--- apps/core/platform-shell-iac/production-environment/main.tf
+++ apps/core/platform-shell-iac/production-environment/main.tf
@@ -31,7 +31,7 @@ module "researchers-peers-svc" {
 # Application Shell
 module "core-platform-shell-browser" {
   source           = "../../../core/platform-shell-browser/iac/production" # The path to the module
-  environment_name = "production"                                          # The deployment environment (branch-name, commit-hash, etc.)
+  environment_name = var.environment_name                                  # The deployment environment (branch-name, commit-hash, etc.)
   vercel_api_token = var.vercel_api_token                                  # The Vercel API token
   # depends_on       = [module.researchers-peers-svc]
 }
@@ -39,7 +39,7 @@ module "core-platform-shell-browser" {
 # Documentation with Docusaurus
 module "dx-dev-docs-browser" {
   source           = "../../../dx/dev-docs-browser/iac/production" # The path to the module
-  environment_name = "production"                                  # The deployment environment (branch-name, commit-hash, etc.)
+  environment_name = var.environment_name                          # The deployment environment (branch-name, commit-hash, etc.)
   vercel_api_token = var.vercel_api_token                          # The Vercel API token
   # depends_on       = [module.researchers-peers-svc]
 }
