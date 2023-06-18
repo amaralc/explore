79850dab41f9110e3226f44ddba08ce79de340ef
amaralc <amaral.calil@gmail.com>
2023-06-18 09:36:30 -0300

    Revert "adjust: remove output"
    This reverts commit 815dd4828fe2581f8d91d242af60e94dc97cc6f5.

--- apps/core/platform-shell-iac/production-environment/outputs.tf
+++ apps/core/platform-shell-iac/production-environment/outputs.tf
@@ -1,4 +1,4 @@
-# output "neon_project_id" {
-#   description = "Neon project ID"
-#   value       = module.postgresql-dbms.neon_project_id
-# }
+output "neon_project_id" {
+  description = "Neon project ID"
+  value       = module.postgresql-dbms.neon_project_id
+}
