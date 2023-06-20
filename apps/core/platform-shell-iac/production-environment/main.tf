67735e8721cc89fd12bc2a0fa5396f8c62fa2766
amaralc <amaral.calil@gmail.com>
2023-06-20 13:33:43 -0300

    chore: remove unused code

--- apps/core/platform-shell-iac/production-environment/main.tf
+++ apps/core/platform-shell-iac/production-environment/main.tf
@@ -1,90 +1,4 @@
-# # Network
-# resource "google_compute_network" "vpc" {
-#   name                    = var.project_id
-#   auto_create_subnetworks = false
-# }
 
-# resource "google_compute_subnetwork" "subnet" {
-#   name          = "${var.project_id}-${var.environment_name}"
-#   ip_cidr_range = "10.0.0.0/16"
-#   region        = "us-central1"
-#   network       = google_compute_network.vpc.self_link
-# }
-
-# # Network
-# resource "google_compute_network" "private_network" {
-#   provider = google-beta
-
-#   name = var.project_id
-# }
-
-# resource "google_compute_global_address" "private_ip_address" {
-#   provider = google-beta
-
-#   name          = "private-ip-address"
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   prefix_length = 16
-#   network       = google_compute_network.private_network.id
-# }
-
-# resource "google_service_networking_connection" "private_vpc_connection" {
-#   provider = google-beta
-
-#   network                 = google_compute_network.private_network.id
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-# }
-
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-# # https://cloud.google.com/run/docs/configuring/connecting-vpc#terraform
-# module "test-vpc-module" {
-#   source       = "terraform-google-modules/network/google"
-#   version      = "~> 7.0"
-#   project_id   = var.project_id # Replace this with your project ID in quotes
-#   network_name = "my-serverless-network"
-#   mtu          = 1460
-
-#   subnets = [
-#     {
-#       subnet_name   = "serverless-subnet"
-#       subnet_ip     = "10.10.10.0/28"
-#       subnet_region = var.region
-#     }
-#   ]
-# }
-
-# module "serverless-connector" {
-#   source     = "terraform-google-modules/network/google//modules/vpc-serverless-connector-beta"
-#   version    = "~> 7.0"
-#   project_id = var.project_id
-#   vpc_connectors = [{
-#     name        = "central-serverless"
-#     region      = var.region
-#     subnet_name = module.test-vpc-module.subnets["${var.region}/serverless-subnet"].name
-#     # host_project_id = var.host_project_id # Specify a host_project_id for shared VPC
-#     machine_type  = "e2-standard-4"
-#     min_instances = 2
-#     max_instances = 7
-#     }
-#     # Uncomment to specify an ip_cidr_range
-#     #   , {
-#     #     name          = "central-serverless2"
-#     #     region        = "us-central1"
-#     #     network       = module.test-vpc-module.network_name
-#     #     ip_cidr_range = "10.10.11.0/28"
-#     #     subnet_name   = null
-#     #     machine_type  = "e2-standard-4"
-#     #     min_instances = 2
-#     #   max_instances = 7 }
-#   ]
-#   # depends_on = [
-#   #   google_project_service.vpcaccess-api
-#   # ]
-# }
 
 # # PostgreSQL Database Management System
 # module "postgresql-dbms" {
@@ -93,12 +7,6 @@ resource "random_id" "db_name_suffix" {
 #   project_id            = var.project_id
 #   neon_api_key          = var.neon_api_key
 # }
-# resource "neon_project" "postgresql-dbms" {
-#   name                     = var.project_id            # Use the same project ID as in the Google Cloud provider
-#   region_id                = var.neon_project_location #"aws-eu-central-1"
-#   autoscaling_limit_max_cu = 1
-# }
-
 
 # Network
 # VPC
@@ -153,6 +61,10 @@ resource "google_compute_router_nat" "router_nat" {
   router                             = google_compute_router.router.name
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
   nat_ip_allocate_option             = "AUTO_ONLY"
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
 }
 
 resource "google_sql_database_instance" "postgresql-dbms" {
