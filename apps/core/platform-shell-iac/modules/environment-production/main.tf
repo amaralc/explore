b45b737840a7d19bad99a0c7fe4edf66d7527dc1
amaralc <amaral.calil@gmail.com>
2023-06-21 09:45:39 -0300

    refactor: enable APIs with terraform

--- apps/core/platform-shell-iac/modules/environment-production/main.tf
+++ apps/core/platform-shell-iac/modules/environment-production/main.tf
@@ -1,15 +1,22 @@
-module "project" {
-  source                 = "../gcp-project"
-  gcp_organization_id    = var.gcp_organization_id
-  gcp_project_name       = var.gcp_project_name
-  gcp_project_id         = var.gcp_project_id
-  gcp_billing_account_id = var.gcp_billing_account_id
+# resource "google_project_service" "resource_manager" {
+#   project            = var.gcp_project_id
+#   service            = "cloudresourcemanager.googleapis.com"
+#   disable_on_destroy = true
+# }
+
+resource "google_project_service" "compute_api" {
+  project                    = var.gcp_project_id
+  service                    = "compute.googleapis.com"
+  disable_on_destroy         = true
+  disable_dependent_services = true
+  # depends_on = [ google_project_service.resource_manager ]
 }
 
-# module "vpc" {
-#   source               = "../gcp-vpc"
-#   environment_name     = "production"
-#   network_name         = "shell"
-#   gcp_project_id       = module.project.id
-#   gcp_project_location = var.gcp_project_location
-# }
+module "vpc" {
+  source               = "../gcp-vpc"
+  environment_name     = "production"
+  network_name         = "shell"
+  gcp_project_id       = var.gcp_project_id
+  gcp_project_location = var.gcp_location
+  depends_on           = [google_project_service.compute_api]
+}
