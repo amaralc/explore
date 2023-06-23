6d3d8414246220b00904fd7e3c37433fa4ce3f49
amaralc <amaral.calil@gmail.com>
2023-06-23 19:52:57 -0300

    fix(PEER-541): adjust backend and versions

--- apps/core/platform-shell-iac/backend.tf
+++ apps/core/platform-shell-iac/backend.tf
@@ -1,9 +1,9 @@
 # This block sets up what backend should be used for Terraform. In this case, we are using Google Cloud Storage.
 terraform {
-  backend "gcs" {                                  # The Google Cloud Storage backend
-    bucket      = "peerlab-platform-shell-tfstate" # The name of the bucket to store the state file
-    credentials = "credentials.json"               # The path to the JSON key file for the Service Account Terraform will use to manage its state
-    prefix      = "experimental"                   # The path to the state file within the bucket
+  backend "gcs" {                                   # The Google Cloud Storage backend
+    bucket      = "core-platform-shell-iac-tfstate" # The name of the bucket to store the state file
+    credentials = "credentials.json"                # The path to the JSON key file for the Service Account Terraform will use to manage its state
+    prefix      = "experimental"                    # The path to the state file within the bucket
   }
 }
 
--- apps/dx/dev-docs-browser/iac/production/versions.tf
+++ apps/dx/dev-docs-browser/iac/production/versions.tf
@@ -1,13 +1,7 @@
 terraform {
   required_providers {
     vercel = {
-      source  = "vercel/vercel"
-      version = "0.13.2"
+      source = "vercel/vercel"
     }
   }
-}
-
-# Vercel provider
-provider "vercel" {
-  api_token = var.vercel_api_token
 }
