2e92ff5ec3e7da6eac7f9211f24e6c2dc34aded5
amaralc <amaral.calil@gmail.com>
2023-06-23 17:37:15 -0300

    refactor: rename and organize setup files

--- apps/core/platform-shell-iac/project-setup.sh
+++ apps/core/platform-shell-iac/project-setup.sh
@@ -23,8 +23,8 @@ GCP_TERRAFORM_STATE_BUCKET_NAME="$GCP_PROJECT_ID-tfstate"
 
 # BASIC GCP SETUP
 
-# Authenticate with Google Cloud
-gcloud auth login
+# # Authenticate with Google Cloud
+# gcloud auth login
 
 # Create project
 gcloud projects create $GCP_PROJECT_ID
@@ -34,6 +34,12 @@ gcloud config set project $GCP_PROJECT_ID
 
 # Enable billing
 gcloud beta billing projects link $GCP_PROJECT_ID --billing-account=$GCP_BILLING_ACCOUNT_ID
+
+# Enable necessary APIs
+gcloud services enable cloudresourcemanager.googleapis.com --project $GCP_PROJECT_ID
+
+# Create a bucket
+gsutil mb -p $GCP_PROJECT_ID -l $GCP_PROJECT_LOCATION gs://$GCP_TERRAFORM_STATE_BUCKET_NAME
 
 # Create a service account
 gcloud iam service-accounts create $GCP_TF_ADMIN_SERVICE_ACCOUNT_NAME --description="Terraform Admin" --display-name=$GCP_TF_ADMIN_SERVICE_ACCOUNT_NAME
@@ -46,10 +52,4 @@ gcloud projects add-iam-policy-binding $GCP_PROJECT_ID --member="serviceAccount:
 gcloud projects add-iam-policy-binding $GCP_PROJECT_ID --member="serviceAccount:$GCP_SERVICE_ACCOUNT_EMAIL" --role="roles/secretmanager.admin"             # Create and manage iam policies
 
 # Create a key for the service account
-gcloud iam service-accounts keys create ./key.json --iam-account $GCP_TF_ADMIN_SERVICE_ACCOUNT_NAME@$GCP_PROJECT_ID.iam.gserviceaccount.com
-
-# Enable APIs
-gcloud services enable cloudresourcemanager.googleapis.com --project $GCP_PROJECT_ID
-
-# Create a bucket
-gsutil mb -p $GCP_PROJECT_ID -l $GCP_PROJECT_LOCATION gs://$GCP_TERRAFORM_STATE_BUCKET_NAME
+gcloud iam service-accounts keys create ./apps/core/platform-shell-iac/credentials.json --iam-account $GCP_SERVICE_ACCOUNT_EMAIL
