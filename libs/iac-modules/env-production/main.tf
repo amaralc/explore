9d0ddd0a54255c3b722853192b8ec18aa21b8e6a
amaralc <amaral.calil@gmail.com>
2023-06-23 18:13:06 -0300

    adjust: fix paths

--- libs/iac-modules/env-production/main.tf
+++ libs/iac-modules/env-production/main.tf
@@ -43,17 +43,17 @@ module "researchers-peers" {
 
 # Application Shell
 module "core-platform-shell-browser" {
-  source           = "../../../core/platform-shell-browser/iac/production" # The path to the module
-  environment_name = var.environment_name                                  # The deployment environment (branch-name, commit-hash, etc.)
-  vercel_api_token = var.vercel_api_token                                  # The Vercel API token
+  source           = "../../../apps/core/platform-shell-browser/iac/production" # The path to the module
+  environment_name = var.environment_name                                       # The deployment environment (branch-name, commit-hash, etc.)
+  vercel_api_token = var.vercel_api_token                                       # The Vercel API token
   depends_on       = [module.researchers-peers]
 }
 
 # Documentation with Docusaurus
 module "dx-dev-docs-browser" {
-  source           = "../../../dx/dev-docs-browser/iac/production" # The path to the module
-  environment_name = var.environment_name                          # The deployment environment (branch-name, commit-hash, etc.)
-  vercel_api_token = var.vercel_api_token                          # The Vercel API token
+  source           = "../../../apps/dx/dev-docs-browser/iac/production" # The path to the module
+  environment_name = var.environment_name                               # The deployment environment (branch-name, commit-hash, etc.)
+  vercel_api_token = var.vercel_api_token                               # The Vercel API token
   depends_on       = [module.researchers-peers]
 }
 
