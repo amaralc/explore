b94866e384f87fa43b4570b34ddcd6c5a7f7d907
amaralc <amaral.calil@gmail.com>
2023-06-23 22:35:30 -0300

    chore: cleanup versions

--- apps/researchers/peers/svc-rest-api/iac/run/versions.tf
+++ apps/researchers/peers/svc-rest-api/iac/run/versions.tf
@@ -1,29 +1,14 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "4.65.2" # Adjust the version to match the latest release
+      source = "hashicorp/google"
     }
 
     google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "4.65.2" # Adjust the version to match the latest release
+      source = "hashicorp/google-beta"
     }
   }
 }
 
 
-# Configure the Google Cloud Provider for Terraform
-provider "google" {
-  credentials = file(var.gcp_credentials_file_path) # The service account key
-  project     = var.project_id                      # Your Google Cloud project ID
-  region      = var.region                          # The region where resources will be created
-}
-
-# The google-beta provider is used for features not yet available in the google provider
-provider "google-beta" {
-  credentials = file(var.gcp_credentials_file_path) # The service account key
-  project     = var.project_id                      # Your Google Cloud project ID
-  region      = var.region                          # The region where resources will be created
-}
 
