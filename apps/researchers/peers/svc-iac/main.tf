144e1a33f06b984beae7346756aa7e6097a99c7c
amaralc <amaral.calil@gmail.com>
2023-06-23 22:50:30 -0300

    adjust: fix names

--- apps/researchers/peers/svc-iac/main.tf
+++ apps/researchers/peers/svc-iac/main.tf
@@ -74,9 +74,9 @@ module "rest-api" {
   docker_image_name                                 = "${local.app_name}-${local.app_component_name}"
   gcp_vpc_access_connector_name                     = var.gcp_vpc_access_connector_name
   gcp_direct_database_connection_url_secret_id      = module.service_secrets.secret_ids[0].secret_id
-  gcp_direct_database_connection_url_secret_version = module.service_secrets.secrets_versions[0].id
+  gcp_direct_database_connection_url_secret_version = module.service_secrets.secrets_versions[0].version_id
   gcp_pooled_database_connection_url_secret_id      = module.service_secrets.secret_ids[1].secret_id
-  gcp_pooled_database_connection_url_secret_version = module.service_secrets.secrets_versions[1].id
+  gcp_pooled_database_connection_url_secret_version = module.service_secrets.secrets_versions[1].version_id
   depends_on                                        = [module.service_account, module.service_account_permissions, module.database_access, module.service_secrets]
 }
 
