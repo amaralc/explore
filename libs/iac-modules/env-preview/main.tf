1ac7227ecd5129c8d66ed944a50b1167081bdaff
amaralc <amaral.calil@gmail.com>
2023-06-25 16:10:02 -0300

    docs: add references in nx graph

--- libs/iac-modules/env-preview/main.tf
+++ libs/iac-modules/env-preview/main.tf
@@ -30,3 +30,37 @@ module "postgresql_dbms" {
   gcp_sql_dbms_source_instance_id = var.source_environment_dbms_instance_id
   depends_on                      = [module.vpc]
 }
+
+# # Researchers Peers Microservice
+# module "researchers-peers" {
+#   source                              = "../../../apps/researchers/peers/svc-iac"
+#   environment_name                    = local.environment_name
+#   gcp_project_id                      = var.gcp_project_id
+#   gcp_location                        = var.gcp_location
+#   short_commit_sha                    = var.short_commit_sha
+#   gcp_docker_artifact_repository_name = var.gcp_docker_artifact_repository_name
+#   gcp_sql_dbms_instance_host          = module.postgresql_dbms.google_sql_database_instance.private_ip_address
+#   gcp_sql_dbms_instance_name          = module.postgresql_dbms.google_sql_database_instance.name
+#   gcp_vpc_access_connector_name       = module.vpc.gcp_vpc_access_connector_name # Necessary to stablish connection with database
+#   depends_on                          = [module.postgresql_dbms]
+# }
+
+# # Application Shell
+# module "core-platform-shell-browser" {
+#   source           = "../../../apps/core/platform-shell-browser/iac/production" # The path to the module
+#   environment_name = var.branch_name                                            # The name of the branch
+#   depends_on       = [module.researchers-peers]
+# }
+
+# # Documentation with Docusaurus
+# module "dx-dev-docs-browser" {
+#   source           = "../../../apps/dx/dev-docs-browser/iac/production" # The path to the module
+#   environment_name = var.branch_name                                    # The name of the branch
+#   depends_on       = [module.researchers-peers]
+# }
+
+# # Nx Graph
+# module "core-root-shell-graph" {
+#   source           = "../../../apps/core/root-shell-graph/iac/production" # The path to the module
+#   environment_name = var.branch_name                                      # The name of the branch
+# }
