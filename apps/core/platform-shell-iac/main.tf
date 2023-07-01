a7c0dc57fcab9364766fabe77c8d0527c1a44cc2
amaralc <amaral.calil@gmail.com>
2023-07-01 11:50:07 -0300

    chore: remove preview environment

--- apps/core/platform-shell-iac/main.tf
+++ apps/core/platform-shell-iac/main.tf
@@ -27,22 +27,25 @@ module "production" {
   depends_on                          = [module.gcp_apis]
 }
 
-# Branch Environment
-module "feature-peer-541-add-docs-to-preview" {
-  source                                                               = "../../../libs/iac-modules/environment"
-  branch_name                                                          = "feature/PEER-541-add-docs-to-preview"
-  environment_name                                                     = "feature-peer-541-add-docs-to-preview" # environment_name=$(echo "$branch_name" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g'). [hypothesis] Passing this value hardcoded here prevents the module from being destroyed and recreated unnecessarily. Take a look at the description of the environment_name variable in the environment module.
-  source_environment_branch_name                                       = module.production.branch_name
-  source_environment_dbms_instance_id                                  = module.production.postgresql_dbms_instance_id
-  short_commit_sha                                                     = var.short_commit_sha
-  gcp_project_id                                                       = var.gcp_project_id
-  gcp_location                                                         = var.gcp_location
-  gcp_docker_artifact_repository_name                                  = var.gcp_docker_artifact_repository_name
-  production_environment_core_platform_shell_browser_vercel_project_id = module.production.core_platform_shell_browser_vercel_project_id
-  production_environment_core_root_shell_graph_vercel_project_id       = module.production.core_root_shell_graph_vercel_project_id
-  production_environment_dx_dev_docs_browser_vercel_project_id         = module.production.dx_dev_docs_browser_vercel_project_id
-  depends_on                                                           = [module.gcp_apis, module.production]
-}
+# TODO -> How to prevent one preview branch from applying changes that disrupt other preview environments?
+# #       Option: create a new project for every preview branch?
+
+# # Branch Environment
+# module "feature-peer-541-add-docs-to-preview" {
+#   source                                                               = "../../../libs/iac-modules/environment"
+#   branch_name                                                          = "feature/PEER-541-add-docs-to-preview"
+#   environment_name                                                     = "feature-peer-541-add-docs-to-preview" # environment_name=$(echo "$branch_name" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g'). [hypothesis] Passing this value hardcoded here prevents the module from being destroyed and recreated unnecessarily. Take a look at the description of the environment_name variable in the environment module.
+#   source_environment_branch_name                                       = module.production.branch_name
+#   source_environment_dbms_instance_id                                  = module.production.postgresql_dbms_instance_id
+#   short_commit_sha                                                     = var.short_commit_sha
+#   gcp_project_id                                                       = var.gcp_project_id
+#   gcp_location                                                         = var.gcp_location
+#   gcp_docker_artifact_repository_name                                  = var.gcp_docker_artifact_repository_name
+#   production_environment_core_platform_shell_browser_vercel_project_id = module.production.core_platform_shell_browser_vercel_project_id
+#   production_environment_core_root_shell_graph_vercel_project_id       = module.production.core_root_shell_graph_vercel_project_id
+#   production_environment_dx_dev_docs_browser_vercel_project_id         = module.production.dx_dev_docs_browser_vercel_project_id
+#   depends_on                                                           = [module.gcp_apis, module.production]
+# }
 
 
 # # Second Approach (GCP)
