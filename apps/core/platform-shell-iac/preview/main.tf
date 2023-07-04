018481756aec5b45d1e3087d242faac8aaad5397
amaralc <amaral.calil@gmail.com>
2023-07-04 08:29:32 -0300

    debug: comment resources

--- apps/core/platform-shell-iac/preview/main.tf
+++ apps/core/platform-shell-iac/preview/main.tf
@@ -20,22 +20,22 @@ locals {
 }
 
 
-# # Branch Environment
-# module "refactor-peer-541-parse-branch-name-within-module" {
-#   source                                                               = "../../../../libs/iac-modules/environment"
-#   branch_name                                                          = "refactor/PEER-541-parse-branch-name-within-module"
-#   owner_account_email                                                  = var.owner_account_email
-#   creator_service_account_email                                        = local.service_account_email
-#   source_environment_branch_name                                       = local.source_environment_branch_name
-#   source_environment_dbms_instance_id                                  = data.google_sql_database_instance.production.id
-#   short_commit_sha                                                     = var.short_commit_sha
-#   gcp_project_id                                                       = var.gcp_project_id
-#   gcp_billing_account_id                                               = var.gcp_billing_account_id
-#   gcp_organization_id                                                  = var.gcp_organization_id
-#   gcp_location                                                         = var.gcp_location
-#   gcp_docker_artifact_repository_name                                  = var.gcp_docker_artifact_repository_name
-#   production_environment_core_platform_shell_browser_vercel_project_id = var.core_platform_shell_browser_vercel_project_id
-#   production_environment_core_root_shell_graph_vercel_project_id       = var.core_root_shell_graph_vercel_project_id
-#   production_environment_dx_dev_docs_browser_vercel_project_id         = var.dx_dev_docs_browser_vercel_project_id
-#   depends_on                                                           = [data.google_sql_database_instance.production]
-# }
+# Branch Environment
+module "refactor-peer-541-parse-branch-name-within-module" {
+  source                                                               = "../../../../libs/iac-modules/environment"
+  branch_name                                                          = "refactor/PEER-541-parse-branch-name-within-module"
+  owner_account_email                                                  = var.owner_account_email
+  creator_service_account_email                                        = local.service_account_email
+  source_environment_branch_name                                       = local.source_environment_branch_name
+  source_environment_dbms_instance_id                                  = data.google_sql_database_instance.production.id
+  short_commit_sha                                                     = var.short_commit_sha
+  gcp_project_id                                                       = var.gcp_project_id
+  gcp_billing_account_id                                               = var.gcp_billing_account_id
+  gcp_organization_id                                                  = var.gcp_organization_id
+  gcp_location                                                         = var.gcp_location
+  gcp_docker_artifact_repository_name                                  = var.gcp_docker_artifact_repository_name
+  production_environment_core_platform_shell_browser_vercel_project_id = var.core_platform_shell_browser_vercel_project_id
+  production_environment_core_root_shell_graph_vercel_project_id       = var.core_root_shell_graph_vercel_project_id
+  production_environment_dx_dev_docs_browser_vercel_project_id         = var.dx_dev_docs_browser_vercel_project_id
+  depends_on                                                           = [data.google_sql_database_instance.production]
+}
