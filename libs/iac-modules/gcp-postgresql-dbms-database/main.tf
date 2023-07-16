3a72d95eb8ac56a1195af39dace7f903bfad0dc4
amaralc <amaral.calil@gmail.com>
2023-07-16 17:57:42 -0300

    refactor: rename export

--- libs/iac-modules/gcp-postgresql-dbms-database/main.tf
+++ libs/iac-modules/gcp-postgresql-dbms-database/main.tf
@@ -2,10 +2,10 @@
 resource "google_sql_database" "instance" {
   name            = var.database_name
   instance        = var.gcp_sql_dbms_instance_name
-  deletion_policy = "ABANDON"
+  deletion_policy = "ABANDON" # TODO -> Is this the best option?
 }
 
-output "instance" {
-  value     = google_sql_database.instance
+output "name" {
+  value     = google_sql_database.instance.name
   sensitive = true
 }
