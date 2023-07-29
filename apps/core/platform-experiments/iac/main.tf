4418a398247b34fba9c30d4566737b52e7b5a03c
amaralc <amaral.calil@gmail.com>
2023-07-29 20:51:22 -0300

    fix(PEER-573): remove unnused arguments

--- apps/core/platform-experiments/iac/main.tf
+++ apps/core/platform-experiments/iac/main.tf
@@ -15,20 +15,16 @@ module "database_and_access_management" {
 }
 
 module "cloud-run-instance" {
-  source                                          = "./cloud-run"
-  docker_image_name                               = local.service_name
-  environment_name                                = var.environment_name
-  gcp_location                                    = var.gcp_location
-  gcp_project_id                                  = var.gcp_project_id
-  short_commit_sha                                = var.short_commit_sha
-  gcp_vpc_access_connector_name                   = var.gcp_vpc_access_connector_name
-  gcp_docker_artifact_repository_name             = var.gcp_docker_artifact_repository_name
-  gcp_sql_dbms_instance_connection_name           = var.gcp_sql_dbms_instance_connection_name
-  gcp_service_account_email                       = module.database_and_access_management.service_account_email
-  gcp_jdbc_database_connection_url_secret_id      = module.database_and_access_management.jdbc_database_url_secret_id
-  gcp_jdbc_database_connection_url_secret_version = module.database_and_access_management.jdbc_database_url_secret_version
-  gcp_dbms_username_secret_id                     = module.database_and_access_management.dbms_username_secret_id
-  gcp_dbms_username_secret_version                = module.database_and_access_management.dbms_username_secret_version
-  gcp_dbms_password_secret_id                     = module.database_and_access_management.dbms_password_secret_id
-  gcp_dbms_password_secret_version                = module.database_and_access_management.dbms_password_secret_version
+  source                                     = "./cloud-run"
+  docker_image_name                          = local.service_name
+  environment_name                           = var.environment_name
+  gcp_location                               = var.gcp_location
+  gcp_project_id                             = var.gcp_project_id
+  short_commit_sha                           = var.short_commit_sha
+  gcp_vpc_access_connector_name              = var.gcp_vpc_access_connector_name
+  gcp_docker_artifact_repository_name        = var.gcp_docker_artifact_repository_name
+  gcp_sql_dbms_instance_connection_name      = var.gcp_sql_dbms_instance_connection_name
+  gcp_service_account_email                  = module.database_and_access_management.service_account_email
+  gcp_database_connection_url_secret_id      = module.database_and_access_management.database_url_secret_id
+  gcp_database_connection_url_secret_version = module.database_and_access_management.database_url_secret_version
 }
