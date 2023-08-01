ea2660250447a02a01f461293b9f24c791f765cb
amaralc <amaral.calil@gmail.com>
2023-08-01 00:40:24 -0300

    adjust: abandon db

--- libs/iac-modules/gcp-postgresql-dbms-database/main.tf
+++ libs/iac-modules/gcp-postgresql-dbms-database/main.tf
@@ -2,7 +2,7 @@
 resource "google_sql_database" "instance" {
   name            = var.database_name
   instance        = var.gcp_sql_dbms_instance_name
-  deletion_policy = "DELETE" #  The deletion policy for the database. Setting ABANDON allows the resource to be abandoned rather than deleted. This is useful for Postgres, where databases cannot be deleted from the API if there are users other than cloudsqlsuperuser with access. Possible values are: "ABANDON", "DELETE". Defaults to "DELETE".
+  deletion_policy = "ABANDON" #  The deletion policy for the database. Setting ABANDON allows the resource to be abandoned rather than deleted. This is useful for Postgres, where databases cannot be deleted from the API if there are users other than cloudsqlsuperuser with access. Possible values are: "ABANDON", "DELETE". Defaults to "DELETE".
 }
 
 output "name" {
