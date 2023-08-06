7c11fe3dcd8954326526afad10ad508a344cb4ce
amaralc <amaral.calil@gmail.com>
2023-08-06 12:08:17 -0300

    chore: cleanup environment

--- apps/core/platform-shell-iac/production/main.tf
+++ apps/core/platform-shell-iac/production/main.tf
@@ -7,18 +7,18 @@ locals {
   service_account_email = local.credentials.client_email
 }
 
-# Production Environment
-module "production" {
-  source                              = "../../../../libs/iac-modules/environment"
-  branch_name                         = "production"
-  short_commit_sha                    = var.short_commit_sha
-  gcp_project_id                      = var.gcp_project_id
-  gcp_location                        = var.gcp_location
-  gcp_docker_artifact_repository_name = var.gcp_docker_artifact_repository_name
-  gcp_billing_account_id              = var.gcp_billing_account_id
-  gcp_organization_id                 = var.gcp_organization_id
-  mongodb_atlas_org_id                = var.mongodb_atlas_org_id
-  support_account_email               = var.support_account_email
-  creator_service_account_email       = local.service_account_email
-  owner_account_email                 = var.owner_account_email
-}
+# # Production Environment
+# module "production" {
+#   source                              = "../../../../libs/iac-modules/environment"
+#   branch_name                         = "production"
+#   short_commit_sha                    = var.short_commit_sha
+#   gcp_project_id                      = var.gcp_project_id
+#   gcp_location                        = var.gcp_location
+#   gcp_docker_artifact_repository_name = var.gcp_docker_artifact_repository_name
+#   gcp_billing_account_id              = var.gcp_billing_account_id
+#   gcp_organization_id                 = var.gcp_organization_id
+#   mongodb_atlas_org_id                = var.mongodb_atlas_org_id
+#   support_account_email               = var.support_account_email
+#   creator_service_account_email       = local.service_account_email
+#   owner_account_email                 = var.owner_account_email
+# }
