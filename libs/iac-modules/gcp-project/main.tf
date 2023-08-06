363533e09badc71c2279b9d80776c01f83a11919
amaralc <amaral.calil@gmail.com>
2023-08-06 10:55:59 -0300

    fix(PEER-579): use random number

--- libs/iac-modules/gcp-project/main.tf
+++ libs/iac-modules/gcp-project/main.tf
@@ -1,23 +1,23 @@
-# # Generate a random ID with the random_id resource. This ID will be used as prefix to create a unique project ID for the new GCP project.
-# resource "random_id" "instance" {
-#   byte_length = 8
-# }
+# Generate a random ID with the random_id resource. This ID will be used as prefix to create a unique project ID for the new GCP project.
+resource "random_id" "instance" {
+  byte_length = 8
+}
 
-# # The null_resource with lifecycle block and ignore_changes argument is used to ensure that the random ID does not change in subsequent runs.
-# resource "null_resource" "ignore_id_changes" {
-#   triggers = {
-#     id = random_id.instance.hex
-#   }
+# The null_resource with lifecycle block and ignore_changes argument is used to ensure that the random ID does not change in subsequent runs.
+resource "null_resource" "ignore_id_changes" {
+  triggers = {
+    id = random_id.instance.hex
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       triggers,
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      triggers,
+    ]
+  }
+}
 
 locals {
-  project_id = var.environment_name # "${substr("${var.environment_name}", 0, 25)}-${substr(random_id.instance.hex, 0, 4)}" # Name cannot have more than 30 characters
+  project_id = "${substr("${var.environment_name}", 0, 25)}-${substr(random_id.instance.hex, 0, 4)}" # Name cannot have more than 30 characters
 }
 
 # Create a project in the GCP organization if the environment is a preview environment
