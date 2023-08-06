7d203edb0868c4e439dd53a6a6d88a0dc4bf2ed4
amaralc <amaral.calil@gmail.com>
2023-08-06 07:15:10 -0300

    fix(PEER-567): disable services and cleanup

--- apps/security/iam-svc/iac/variables.tf
+++ apps/security/iam-svc/iac/variables.tf
@@ -1,70 +1,9 @@
-# variable "source_environment_branch_name" {
-#   description = "The name of the source environment branch"
-#   type        = string
-#   default     = null
-# }
-
-# variable "environment_name" {
-#   description = "The preview environment unique name (e.g. branch-name, commit-hash, etc.)"
-#   type        = string
-# }
-
-# variable "gcp_sql_dbms_instance_name" {
-#   description = "The name of the dbms instance"
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "gcp_sql_dbms_instance_host" {
-#   description = "The host of the database instance"
-#   type        = string
-#   sensitive   = true
-# }
-
 variable "gcp_project_id" {
   description = "The Google Cloud project ID"
   type        = string
-}
-
-variable "support_account_email" {
-  description = "The support email for the IAP brand"
-  type        = string
-  sensitive   = true
 }
 
 variable "application_title" {
   description = "The title of the application"
   type        = string
 }
-
-# variable "gcp_customer_id" {
-#   description = "The Google Cloud customer ID"
-#   type        = string
-#   sensitive   = true
-# }
-
-
-# variable "short_commit_sha" {
-#   description = "The commit short SHA of the source code to deploy"
-#   type        = string
-# }
-
-# variable "gcp_location" {
-#   description = "The region where resources will be created"
-#   type        = string
-# }
-
-# variable "gcp_vpc_access_connector_name" {
-#   description = "The name of the VPC access connector"
-#   type        = string
-# }
-
-# variable "gcp_docker_artifact_repository_name" {
-#   description = "The name of the Docker repository"
-#   type        = string
-# }
-
-# variable "gcp_sql_dbms_instance_connection_name" {
-#   description = "The connection name of the Cloud SQL DBMS instance"
-#   type        = string
-# }
--- libs/iac-modules/environment/main.tf
+++ libs/iac-modules/environment/main.tf
@@ -14,7 +14,7 @@ locals {
 # Create child projects for each environment (downsides: more projects to manage, more billing accounts to manage)
 module "gcp_project" {
   source                        = "../gcp-project"
-  count                         = 0 # local.is_production_environment ? 0 : 1 # For now, we wont use child projects in order to avoid billing account issues
+  count                         = local.is_production_environment ? 0 : 0 # For now, we wont use child projects in order to avoid billing account issues
   is_production_environment     = local.is_production_environment
   gcp_billing_account_id        = var.gcp_billing_account_id
   gcp_organization_id           = var.gcp_organization_id
@@ -53,8 +53,13 @@ module "gcp_apis" {
   ]
 }
 
+locals {
+  is_gcp_sql_instance_enabled = local.is_production_environment ? 0 : 0 # Weather to enable a SQL instance or not
+}
+
 # Create the main Virtual Private Cloud (VPC)
 module "vpc" {
+  count            = local.is_production_environment ? 1 : 1 # Enabled in production and preview environments
   source           = "../gcp-vpc"
   environment_name = local.short_environment_name # Limit the name to 24 characters
   gcp_project_id   = local.project_id
@@ -68,6 +73,7 @@ output "vpc" {
 
 # Create a PostgreSQL database management system (DBMS) instance clone for the preview environment
 module "postgresql_dbms" {
+  count                           = local.is_production_environment ? 0 : 0
   source                          = "../gcp-postgresql-dbms-environment"
   environment_name                = local.short_environment_name
   gcp_project_id                  = local.project_id
@@ -80,7 +86,7 @@ module "postgresql_dbms" {
 }
 
 output "postgresql_dbms_instance_id" {
-  value = module.postgresql_dbms.gcp_sql_dbms_instance_id
+  value = module.postgresql_dbms[0].gcp_sql_dbms_instance_id
 }
 
 module "mongodb_dbms" {
@@ -93,25 +99,15 @@ module "mongodb_dbms" {
 
 # Identity and Access Management (IAM) Service
 module "security-iam-svc" {
-  count                 = local.is_production_environment ? 1 : 0 # Disabled module
-  source                = "../../../apps/security/iam-svc/iac"
-  gcp_project_id        = local.project_id
-  support_account_email = "contact@amaralc.com"
-  application_title     = local.is_production_environment ? "core-platform-shell-iac-consent" : "core-platform-shell-iac-consent-preview ${local.short_environment_name}"
-  depends_on            = [module.gcp_apis, module.gcp_project]
-  # source_environment_branch_name        = var.source_environment_branch_name # Informs the type of environment in order to decide how to treat database and users
-  # environment_name                      = local.short_environment_name
-  # gcp_location                          = var.gcp_location
-  # short_commit_sha                      = var.short_commit_sha
-  # gcp_docker_artifact_repository_name   = var.gcp_docker_artifact_repository_name
-  # gcp_sql_dbms_instance_host            = module.postgresql_dbms.gcp_sql_dbms_instance_host
-  # gcp_sql_dbms_instance_name            = module.postgresql_dbms.gcp_sql_dbms_instance_name
-  # gcp_vpc_access_connector_name         = module.vpc.gcp_vpc_access_connector_name # Necessary to stablish connection with database
-  # gcp_sql_dbms_instance_connection_name = module.postgresql_dbms.gcp_sql_dbms_instance_connection_name
+  count             = local.is_production_environment ? 1 : 0 # Enabled in production environments only
+  source            = "../../../apps/security/iam-svc/iac"
+  gcp_project_id    = local.project_id
+  application_title = local.is_production_environment ? "core-platform-shell-iac-consent" : "core-platform-shell-iac-consent-preview ${local.short_environment_name}"
+  depends_on        = [module.gcp_apis, module.gcp_project]
 }
 
 module "core-platform-experiments" {
-  count                                 = local.is_production_environment ? 1 : 0 # Enable module only in preview environments
+  count                                 = local.is_production_environment ? 0 : 0 # Enabled in production
   source                                = "../../../apps/core/platform-experiments/iac"
   source_environment_branch_name        = var.source_environment_branch_name # Informs the type of environment in order to decide how to treat database and users
   environment_name                      = local.short_environment_name
@@ -129,7 +125,7 @@ module "core-platform-experiments" {
 # Researchers Peers Microservice
 module "researchers-peers" {
   source                              = "../../../apps/researchers/peers/svc-iac"
-  count                               = local.is_production_environment ? 1 : 0 # Disable module in preview environments
+  count                               = local.is_production_environment ? 0 : 0 # Disable module in preview environments
   source_environment_branch_name      = var.source_environment_branch_name      # Informs the type of environment in order to decide how to treat database and users
   environment_name                    = local.short_environment_name
   gcp_project_id                      = local.project_id
@@ -142,76 +138,76 @@ module "researchers-peers" {
   depends_on                          = [module.postgresql_dbms, module.gcp_apis, module.gcp_project]
 }
 
-# # Nx Graph
-# module "core-platform-shell-browser-vite" {
-#   source                           = "../environment-vercel"
-#   count                            = local.is_production_environment ? 1 : 0 # Disable module in preview environments
-#   project_name                     = "core-platform-shell-browser-vite"
-#   framework                        = "vite"
-#   git_provider                     = "github"
-#   username_and_repository          = "amaralc/peerlab"
-#   branch_name                      = var.branch_name
-#   is_production_environment        = local.is_production_environment
-#   install_command                  = local.is_production_environment ? "pnpm install" : null
-#   build_command                    = local.is_production_environment ? "pnpm nx build core-platform-shell-browser-vite --prod" : null # Use build-graph command to prevent building dependencies used for graphing purposes only
-#   output_directory                 = local.is_production_environment ? "dist/apps/core/platform-shell-browser-vite" : null            # Attention to the output of non-nextjs projects
-#   ignore_command                   = local.is_production_environment ? null : null                                                    # "if [ $VERCEL_ENV == 'production' ]; then exit 1; else exit 0; fi" : null
-#   preview_environment_variables    = local.is_production_environment ? null : null                                                    # Map of string key and values
-#   production_environment_variables = local.is_production_environment ? null : null                                                    # Set of objects with key, value and target (production, preview, development)
-#   source_environment_project_id    = var.production_environment_core_platform_shell_browser_vite_vercel_project_id
+# Nx Graph
+module "core-platform-shell-browser-vite" {
+  source                           = "../environment-vercel"
+  count                            = local.is_production_environment ? 0 : 0 # Disable module
+  project_name                     = "core-platform-shell-browser-vite"
+  framework                        = "vite"
+  git_provider                     = "github"
+  username_and_repository          = "amaralc/peerlab"
+  branch_name                      = var.branch_name
+  is_production_environment        = local.is_production_environment
+  install_command                  = local.is_production_environment ? "pnpm install" : null
+  build_command                    = local.is_production_environment ? "pnpm nx build core-platform-shell-browser-vite --prod" : null # Use build-graph command to prevent building dependencies used for graphing purposes only
+  output_directory                 = local.is_production_environment ? "dist/apps/core/platform-shell-browser-vite" : null            # Attention to the output of non-nextjs projects
+  ignore_command                   = local.is_production_environment ? null : null                                                    # "if [ $VERCEL_ENV == 'production' ]; then exit 1; else exit 0; fi" : null
+  preview_environment_variables    = local.is_production_environment ? null : null                                                    # Map of string key and values
+  production_environment_variables = local.is_production_environment ? null : null                                                    # Set of objects with key, value and target (production, preview, development)
+  source_environment_project_id    = var.production_environment_core_platform_shell_browser_vite_vercel_project_id
 
-#   depends_on = [module.researchers-peers]
-# }
+  depends_on = [module.researchers-peers]
+}
 
-# output "core_platform_shell_browser_vite_vercel_project_id" {
-#   value = local.is_production_environment ? module.core-platform-shell-browser-vite[0].vercel_project_id : null
-# }
+output "core_platform_shell_browser_vite_vercel_project_id" {
+  value = local.is_production_environment ? module.core-platform-shell-browser-vite[0].vercel_project_id : null
+}
 
-# # Documentation with Docusaurus
-# module "dx-dev-docs-browser" {
-#   source                           = "../environment-vercel"
-#   count                            = local.is_production_environment ? 1 : 0 # Disable module in preview environments
-#   project_name                     = "dx-dev-docs-browser"
-#   framework                        = null # https://vercel.com/docs/rest-api/endpoints#create-a-new-project
-#   git_provider                     = "github"
-#   username_and_repository          = "amaralc/peerlab"
-#   branch_name                      = var.branch_name
-#   is_production_environment        = local.is_production_environment
-#   install_command                  = local.is_production_environment ? "pnpm install" : null
-#   dev_command                      = local.is_production_environment ? "pnpm nx serve dx-dev-docs-browser" : null             # Vercel dev command
-#   build_command                    = local.is_production_environment ? "pnpm nx build-docs dx-dev-docs-browser --prod" : null # Use build-docs command to prevent building dependencies used for graphing purposes only
-#   output_directory                 = local.is_production_environment ? "dist/apps/dx/dev-docs-browser" : null                 # Attention to the output of non-nextjs projects
-#   ignore_command                   = local.is_production_environment ? null : null                                            # "if [ $VERCEL_ENV == 'production' ]; then exit 1; else exit 0; fi" : null
-#   preview_environment_variables    = local.is_production_environment ? null : null                                            # Map of string key and values
-#   production_environment_variables = local.is_production_environment ? null : null                                            # Set of objects with key, value and target (production, preview, development)
-#   source_environment_project_id    = var.production_environment_dx_dev_docs_browser_vercel_project_id
+# Documentation with Docusaurus
+module "dx-dev-docs-browser" {
+  source                           = "../environment-vercel"
+  count                            = local.is_production_environment ? 0 : 0 # Disable module
+  project_name                     = "dx-dev-docs-browser"
+  framework                        = null # https://vercel.com/docs/rest-api/endpoints#create-a-new-project
+  git_provider                     = "github"
+  username_and_repository          = "amaralc/peerlab"
+  branch_name                      = var.branch_name
+  is_production_environment        = local.is_production_environment
+  install_command                  = local.is_production_environment ? "pnpm install" : null
+  dev_command                      = local.is_production_environment ? "pnpm nx serve dx-dev-docs-browser" : null             # Vercel dev command
+  build_command                    = local.is_production_environment ? "pnpm nx build-docs dx-dev-docs-browser --prod" : null # Use build-docs command to prevent building dependencies used for graphing purposes only
+  output_directory                 = local.is_production_environment ? "dist/apps/dx/dev-docs-browser" : null                 # Attention to the output of non-nextjs projects
+  ignore_command                   = local.is_production_environment ? null : null                                            # "if [ $VERCEL_ENV == 'production' ]; then exit 1; else exit 0; fi" : null
+  preview_environment_variables    = local.is_production_environment ? null : null                                            # Map of string key and values
+  production_environment_variables = local.is_production_environment ? null : null                                            # Set of objects with key, value and target (production, preview, development)
+  source_environment_project_id    = var.production_environment_dx_dev_docs_browser_vercel_project_id
 
-#   depends_on = [module.researchers-peers]
-# }
+  depends_on = [module.researchers-peers]
+}
 
-# output "dx_dev_docs_browser_vercel_project_id" {
-#   value = local.is_production_environment ? module.dx-dev-docs-browser[0].vercel_project_id : null
-# }
+output "dx_dev_docs_browser_vercel_project_id" {
+  value = local.is_production_environment ? module.dx-dev-docs-browser[0].vercel_project_id : null
+}
 
-# # Nx Graph
-# module "core-root-shell-graph" {
-#   source                           = "../environment-vercel"
-#   count                            = local.is_production_environment ? 1 : 0 # Disable module in preview environments
-#   project_name                     = "core-root-shell-graph"
-#   framework                        = null
-#   git_provider                     = "github"
-#   username_and_repository          = "amaralc/peerlab"
-#   branch_name                      = var.branch_name
-#   is_production_environment        = local.is_production_environment
-#   install_command                  = local.is_production_environment ? "pnpm install" : null
-#   build_command                    = local.is_production_environment ? "pnpm nx build-graph core-root-shell-graph --prod" : null # Use build-graph command to prevent building dependencies used for graphing purposes only
-#   output_directory                 = local.is_production_environment ? "dist/apps/core/root-shell-graph" : null                  # Attention to the output of non-nextjs projects
-#   ignore_command                   = local.is_production_environment ? null : null                                               # "if [ $VERCEL_ENV == 'production' ]; then exit 1; else exit 0; fi" : null
-#   preview_environment_variables    = local.is_production_environment ? null : null                                               # Map of string key and values
-#   production_environment_variables = local.is_production_environment ? null : null                                               # Set of objects with key, value and target (production, preview, development)
-#   source_environment_project_id    = var.production_environment_core_root_shell_graph_vercel_project_id
-# }
+# Nx Graph
+module "core-root-shell-graph" {
+  source                           = "../environment-vercel"
+  count                            = local.is_production_environment ? 0 : 0 # Disable module
+  project_name                     = "core-root-shell-graph"
+  framework                        = null
+  git_provider                     = "github"
+  username_and_repository          = "amaralc/peerlab"
+  branch_name                      = var.branch_name
+  is_production_environment        = local.is_production_environment
+  install_command                  = local.is_production_environment ? "pnpm install" : null
+  build_command                    = local.is_production_environment ? "pnpm nx build-graph core-root-shell-graph --prod" : null # Use build-graph command to prevent building dependencies used for graphing purposes only
+  output_directory                 = local.is_production_environment ? "dist/apps/core/root-shell-graph" : null                  # Attention to the output of non-nextjs projects
+  ignore_command                   = local.is_production_environment ? null : null                                               # "if [ $VERCEL_ENV == 'production' ]; then exit 1; else exit 0; fi" : null
+  preview_environment_variables    = local.is_production_environment ? null : null                                               # Map of string key and values
+  production_environment_variables = local.is_production_environment ? null : null                                               # Set of objects with key, value and target (production, preview, development)
+  source_environment_project_id    = var.production_environment_core_root_shell_graph_vercel_project_id
+}
 
-# output "core_root_shell_graph_vercel_project_id" {
-#   value = local.is_production_environment ? module.core-root-shell-graph[0].vercel_project_id : null
-# }
+output "core_root_shell_graph_vercel_project_id" {
+  value = local.is_production_environment ? module.core-root-shell-graph[0].vercel_project_id : null
+}
