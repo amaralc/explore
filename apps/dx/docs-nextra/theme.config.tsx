eac06a7aec0b006da0fa376e6e3663659d416751
amaralc <amaral.calil@gmail.com>
2023-08-09 01:22:58 -0300

    Revert "chore: remove nextra docs"
    This reverts commit efd1095893b8d0d1118d0cdc8b4a06982bbb7a74.

---
+++ apps/dx/docs-nextra/theme.config.tsx
@@ -0,0 +1,9 @@
+const themeConfig = {
+  project: {
+    link: 'https://github.com/amaralc/peerlab/blob/production/apps/docs',
+  },
+  logo: <strong>Peerlab</strong>,
+  docsRepositoryBase: 'https://github.com/amaralc/peerlab/blob/production/apps/docs/pages',
+};
+
+export default themeConfig;
