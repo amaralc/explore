4a5e9a33f97b3039c48ae3dcce864ff1364e1c3f
amaralc <amaral.calil@gmail.com>
2023-08-12 12:07:29 -0300

    refactor: move docs to module

--- libs/iac-modules/environment/main.tf
+++ libs/iac-modules/environment/main.tf
@@ -153,31 +153,16 @@ module "core-platform-shell-browser-vite" {
   depends_on                    = [module.researchers-peers]
 }
 
-# Documentation with Docusaurus
+# Docs
 module "dx-dev-docs-browser" {
-  source                           = "../environment-vercel"
-  count                            = local.is_production_environment ? 1 : 0 # Disable module
-  project_name                     = "dx-dev-docs-browser"
-  framework                        = null # https://vercel.com/docs/rest-api/endpoints#create-a-new-project
-  git_provider                     = "github"
-  username_and_repository          = "amaralc/peerlab"
-  branch_name                      = var.branch_name
-  is_production_environment        = local.is_production_environment
-  install_command                  = local.is_production_environment ? "pnpm install" : null
-  dev_command                      = local.is_production_environment ? "pnpm nx serve dx-dev-docs-browser" : null             # Vercel dev command
-  build_command                    = local.is_production_environment ? "pnpm nx build-docs dx-dev-docs-browser --prod" : null # Use build-docs command to prevent building dependencies used for graphing purposes only
-  output_directory                 = local.is_production_environment ? "dist/apps/dx/dev-docs-browser" : null                 # Attention to the output of non-nextjs projects
-  ignore_command                   = local.is_production_environment ? null : null                                            # "if [ $VERCEL_ENV == 'production' ]; then exit 1; else exit 0; fi" : null
-  preview_environment_variables    = local.is_production_environment ? null : null                                            # Map of string key and values
-  production_environment_variables = local.is_production_environment ? null : null                                            # Set of objects with key, value and target (production, preview, development)
-  source_environment_project_id    = var.production_environment_dx_dev_docs_browser_vercel_project_id
-
-  depends_on = [module.researchers-peers]
+  source                        = "../../../apps/dx/dev-docs-browser/iac"
+  is_service_enabled            = false
+  is_production_environment     = local.is_production_environment
+  branch_name                   = var.branch_name
+  source_environment_project_id = var.production_environment_dx_dev_docs_browser_vercel_project_id
+  depends_on                    = [module.researchers-peers]
 }
 
-output "dx_dev_docs_browser_vercel_project_id" {
-  value = local.is_production_environment && length(module.dx-dev-docs-browser) > 0 ? module.dx-dev-docs-browser[0].vercel_project_id : null
-}
 
 # Nx Graph
 module "core-root-shell-graph" {
