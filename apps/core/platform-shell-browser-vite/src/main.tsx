ebbfd00b3ed85edd94438f320a3b3e43438f9b4b
amaralc <amaral.calil@gmail.com>
2023-08-12 05:50:54 -0300

    feat(PEER-567): ui - use public assets

--- apps/core/platform-shell-browser-vite/src/main.tsx
+++ apps/core/platform-shell-browser-vite/src/main.tsx
@@ -1,26 +1,28 @@
-import { getAuth } from 'firebase/auth';
-import React, { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './app/app';
-import { unleashConfig } from './config';
-import { firebaseApp } from './libs/firebase';
+// import { getAuth } from 'firebase/auth';
+// import React, { StrictMode } from 'react';
+// import * as ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './app/app';
+// import { unleashConfig } from './config';
+// import { firebaseApp } from './libs/firebase';
+// import '@peerlab/modules/core-platform-shell-browser-vite/global.css';
+import '@peerlab/modules/core-platform-shell-browser-vite/main';
 
-// Lazy load the FlagProvider component
-const FlagProviderLazy = React.lazy(() =>
-  import('@unleash/proxy-client-react').then((module) => ({ default: module.FlagProvider }))
-);
+// // Lazy load the FlagProvider component
+// const FlagProviderLazy = React.lazy(() =>
+//   import('@unleash/proxy-client-react').then((module) => ({ default: module.FlagProvider }))
+// );
 
-const auth = getAuth(firebaseApp);
-console.log('auth', auth);
+// const auth = getAuth(firebaseApp);
+// console.log('auth', auth);
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <StrictMode>
-    <FlagProviderLazy config={unleashConfig}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </FlagProviderLazy>
-  </StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <StrictMode>
+//     <FlagProviderLazy config={unleashConfig}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </FlagProviderLazy>
+//   </StrictMode>
+// );
