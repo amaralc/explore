99bb859db5af6ded6f8d4886eecf823e61c633a4
amaralc <amaral.calil@gmail.com>
2023-08-13 00:11:30 -0300

    feat(PEER-567): use shadcn auth page

--- apps/core/platform-shell-browser-vite/src/app/app.tsx
+++ apps/core/platform-shell-browser-vite/src/app/app.tsx
@@ -1,39 +1,17 @@
-import styled from '@emotion/styled';
-import { Button } from '@peerlab/ui/shared/components/ui/button';
 import { lazy } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import { Route, Routes } from 'react-router-dom';
 
 const SignInPage = lazy(() => import('../pages/sign-in'));
+const AuthenticationPage = lazy(() => import('../pages/auth/sign-in'));
 const RootPage = lazy(() => import('../pages/root'));
 
 export function App() {
   return (
-    <StyledApp>
-      <h1>Welcome!</h1>
-      <Button>Click Me</Button>
-
-      <Routes>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/" element={<RootPage />} />
-      </Routes>
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sign-in">SignIn</Link>
-          </li>
-        </ul>
-      </div>
-    </StyledApp>
+    <Routes>
+      <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/auth/sign-in" element={<AuthenticationPage />} />
+      <Route path="/" element={<RootPage />} />
+    </Routes>
   );
 }
 
--- apps/core/platform-shell-browser-vite/src/main.tsx
+++ apps/core/platform-shell-browser-vite/src/main.tsx
@@ -1,16 +1,16 @@
 import { getAuth } from 'firebase/auth';
-import React, { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
-import { unleashConfig } from './config';
+// import { unleashConfig } from './config';
 import './index.css';
 import { firebaseApp } from './libs/firebase';
 
-// Lazy load the FlagProvider component
-const FlagProviderLazy = React.lazy(() =>
-  import('@unleash/proxy-client-react').then((module) => ({ default: module.FlagProvider })),
-);
+// // Lazy load the FlagProvider component
+// const FlagProviderLazy = React.lazy(() =>
+//   import('@unleash/proxy-client-react').then((module) => ({ default: module.FlagProvider })),
+// );
 
 const auth = getAuth(firebaseApp);
 console.log('auth', auth);
@@ -18,10 +18,10 @@ console.log('auth', auth);
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <FlagProviderLazy config={unleashConfig}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </FlagProviderLazy>
+    {/* <FlagProviderLazy config={unleashConfig}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </FlagProviderLazy> */}
   </StrictMode>,
 );
