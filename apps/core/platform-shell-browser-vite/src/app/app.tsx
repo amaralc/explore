f15bf6f150b384e4519fafb8f67d38aa97017384
amaralc <amaral.calil@gmail.com>
2023-08-13 00:20:21 -0300

    refactor: move shadcn experiment

--- apps/core/platform-shell-browser-vite/src/app/app.tsx
+++ apps/core/platform-shell-browser-vite/src/app/app.tsx
@@ -2,14 +2,12 @@ import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const SignInPage = lazy(() => import('../pages/sign-in'));
-const AuthenticationPage = lazy(() => import('../pages/auth/sign-in'));
 const RootPage = lazy(() => import('../pages/root'));
 
 export function App() {
   return (
     <Routes>
       <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/auth/sign-in" element={<AuthenticationPage />} />
       <Route path="/" element={<RootPage />} />
     </Routes>
   );
