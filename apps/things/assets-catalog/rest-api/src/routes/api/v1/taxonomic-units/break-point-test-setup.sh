0f90d8851edfc1958643d108bd0f2a8292601a56
amaralc <amaral.calil@gmail.com>
2024-05-04 09:13:41 -0300

    test: adjust test setup

--- apps/things/assets-catalog/rest-api/src/routes/api/v1/taxonomic-units/break-point-test-setup.sh
+++ apps/things/assets-catalog/rest-api/src/routes/api/v1/taxonomic-units/break-point-test-setup.sh
@@ -1,11 +1,6 @@
 # Cleanup database
 docker compose down mongodb -v
 
-# Build app
-pnpm nx build things-assets-catalog-rest-api --skip-nx-cache
-cp apps/things/assets-catalog/rest-api/.env.example dist/apps/things/assets-catalog/rest-api/.env
-echo "MONGODB_DATABASE_URI=mongodb://local-root-user:local-root-password@mongodb:27017?retryWrites=true&w=majority" > dist/apps/things/assets-catalog/rest-api/.env
-
 # Setup
 docker compose up mongodb things-assets-catalog-rest-api -d
 docker compose logs --tail=50 --follow things-assets-catalog-rest-api
--- scripts/tests/setup-test-monitoring.sh
+++ scripts/tests/setup-test-monitoring.sh
@@ -1,5 +1,13 @@
 # Setup env
+pnpm nx build things-assets-catalog-rest-api
 cp apps/things/assets-catalog/rest-api/.env.example dist/apps/things/assets-catalog/rest-api/.env
+
+echo "" >> dist/apps/things/assets-catalog/rest-api/.env
+echo "" >> dist/apps/things/assets-catalog/rest-api/.env
+
+echo "# Use mongodb docker host" >> dist/apps/things/assets-catalog/rest-api/.env
+echo "MONGODB_DATABASE_URI=mongodb://local-root-user:local-root-password@mongodb:27017?retryWrites=true&w=majority" >> dist/apps/things/assets-catalog/rest-api/.env
+echo "DATABASE_SEED=false" >> dist/apps/things/assets-catalog/rest-api/.env
 
 # Cleanup
 docker compose down grafana prometheus -v
