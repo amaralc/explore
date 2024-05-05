a6f37621d42c4b63b1517dc3160df38c6df563ac
amaralc <amaral.calil@gmail.com>
2024-05-05 13:01:45 -0300

    test(PEER-694): adjust load tests and file structure

--- apps/things/assets-catalog/rest-api/src/routes/api/v1/taxonomic-units/break-point-test-setup.sh
+++
@@ -1,6 +0,0 @@
-# Cleanup database
-docker compose down mongodb -v
-
-# Setup
-docker compose up mongodb things-assets-catalog-rest-api -d
-docker compose logs --tail=50 --follow things-assets-catalog-rest-api
