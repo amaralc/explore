580819dcb45c7d5c25eea8d29b5e85a0badb6c8a
amaralc <amaral.calil@gmail.com>
2026-02-03 12:37:49 -0300

    chore: [nx migration] set-inject-document-domain

--- teams/archive/platform-shell-browser/cypress.config.ts
+++ teams/archive/platform-shell-browser/cypress.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
     // baseUrl: 'http://localhost:4200',
     video: false,
     screenshotOnRunFailure: false,
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
   },
 });
