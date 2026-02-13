d5ba8311e814cd52cb5786f017f31aa8774ee938
amaralc <amaral.calil@gmail.com>
2026-02-13 14:14:31 -0300

    chore(scripts): remove archived scripts

--- scripts/gcloud/archive/container-registry-auth.sh
+++
@@ -1,8 +0,0 @@
-# Set the path to your service account key file
-export GOOGLE_APPLICATION_CREDENTIALS="teams/kernel-iac/production/credentials.json"
-
-# Authenticate to Google Cloud with the service account
-gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-
-# Configure Docker to use gcloud as a credential helper for GCR
-gcloud auth configure-docker
